8ad484ea90eb3433c15f5eef4943e075fb412069
Weilong <fan01856472@gmail.com>
2022-06-25 18:15:55 +0800

    enemy_location_list

--- web.rb
+++ web.rb
@@ -12,19 +12,30 @@ get '/' do
 end
 
 post '/' do
-  current_status = http_response["arena"]["state"][my_url]
-
   if current_status["wasHit"]
     ["F", "L", "R"].sample
   else
-    "T"
+    # puts enemy_location_list
+    ["F", "L", "R", "T", "T", "T"].sample
   end
 end
 
-def http_response
-  @_http_response ||= JSON.parse(request.body.read)
+# def enemy_location_list
+#   @_enemy_location_list ||= begin
+#     http_response["arena"]["state"].each_with_object do |(player, status), array|
+#       next if player == my_url
+#       array << status.slice("x", "y").values
+#   end
+# end
+
+def current_status
+  @_current_status ||= http_response["arena"]["state"][my_url]
 end
 
 def my_url
   @_my_url ||= http_response["_links"]["self"]["href"]
+end
+
+def http_response
+  @_http_response ||= JSON.parse(request.body.read)
 end
